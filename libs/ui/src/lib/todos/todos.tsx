364599729b303499c1bf6fe584eccc6bb08d2f9d
Annie Huang <annie.hn@gmail.com>
2020-01-18 20:49:17 +1100

    nx g @nrwl/react:component todos --project=ui

---
+++ libs/ui/src/lib/todos/todos.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './todos.css';
+
+/* eslint-disable-next-line */
+export interface TodosProps {}
+
+export const Todos = (props: TodosProps) => {
+  return (
+    <div>
+      <h1>Welcome to todos component!</h1>
+    </div>
+  );
+};
+
+export default Todos;
