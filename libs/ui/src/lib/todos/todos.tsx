585f4a86988a40b21847cc67363f7cbae0a1f9fe
Annie Huang <annie.hn@gmail.com>
2020-01-18 20:55:03 +1100

    Implement todos.tsx library component

--- libs/ui/src/lib/todos/todos.tsx
+++ libs/ui/src/lib/todos/todos.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
 import './todos.css';
+import {Todo} from '@myorg3/data';
 
 /* eslint-disable-next-line */
 export interface TodosProps {}
 
-export const Todos = (props: TodosProps) => {
+// export const Todos = (props: TodosProps) => {
+//   return (
+//     <div>
+//       <h1>Welcome to todos component!</h1>
+//     </div>
+//   );
+// };
+
+export const Todos = (props: { todos: Todo[] }) => {
   return (
-    <div>
-      <h1>Welcome to todos component!</h1>
-    </div>
+    <ul>
+      {props.todos.map((t,i) => (
+        <li className={'todo'} key={i}>{t.title}</li>
+      ))}
+    </ul>
   );
 };
-
 export default Todos;
