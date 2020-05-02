602fd69e68c77969416f076c4b250889ec338555
Annie Huang <annie.hn@gmail.com>
2020-05-02 11:31:14 +1000

    npx nx generate @nrwl/react:lib testlib

---
+++ libs/testlib/src/lib/testlib.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './testlib.css';
+
+/* eslint-disable-next-line */
+export interface TestlibProps {}
+
+export const Testlib = (props: TestlibProps) => {
+  return (
+    <div>
+      <h1>Welcome to testlib component!</h1>
+    </div>
+  );
+};
+
+export default Testlib;
